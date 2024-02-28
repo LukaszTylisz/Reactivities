e705ddb5d97c054fd7fc7e522c518907abfc5828
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-02-28 13:06:28 +0100

    Add generic types, use interceptors

--- frontend/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ frontend/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,16 +14,19 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({activities, selectActivity, deleteActivity,
-    selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit} : Props) {
+export default function ActivityDashboard({activities, selectActivity, deleteActivity, submitting,
+        selectedActivity, cancelSelectActivity, editMode, openForm,
+        closeForm, createOrEdit} : Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} 
                 selectActivity={selectActivity}
-                deleteActivity={deleteActivity} />
+                deleteActivity={deleteActivity} 
+                submitting= {submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -33,7 +36,12 @@ export default function ActivityDashboard({activities, selectActivity, deleteAct
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm
+                    closeForm={closeForm}
+                    activity={selectedActivity}
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}     
+                />}
             </Grid.Column>
         </Grid>
     )
