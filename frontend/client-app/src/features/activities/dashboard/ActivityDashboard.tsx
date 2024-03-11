f7e14a82afd9bf63693c914fe1a45a5f3b6582ce
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-03-11 14:47:00 +0100

    Adding Routes to project

--- frontend/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ frontend/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,10 +4,18 @@ import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from './ActivityList';
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
     
     return (
         <Grid>
