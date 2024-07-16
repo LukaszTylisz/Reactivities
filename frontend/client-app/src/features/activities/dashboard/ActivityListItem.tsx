6462c69a5b7dd1a223bed2165ddf317187dc8065
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-07-16 14:31:49 +0200

    Added Formik and modified reusable form inputs

--- frontend/client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ frontend/client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,7 @@
-import { Button, Icon, Item, Segment } from "semantic-ui-react"
-import { Activity } from "../../../app/models/activity"
-import { Link } from "react-router-dom"
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { Link } from "react-router-dom";
+import { format } from 'date-fns';
 
 interface Props {
     activity: Activity
@@ -23,7 +24,7 @@ export function ActivityListItem({ activity }: Props) {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> {activity.date}
+                    <Icon name='clock' /> {format(activity.date!, 'dd MM yyyy h:mm aa')}
                     <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
