4da2342da89dea80d4f557dee9cca28e94e2ad04
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-07-17 10:57:54 +0200

    Added Identity

---
+++ backend/Reactivities.Api/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.Api.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+        })
+        .AddEntityFrameworkStores<DataContext>();
+
+        services.AddAuthentication();
+
+        return services;
+    }
+}
