b330b463266756b0c97c658974a1f897b4b1ceb6
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-07-18 10:45:28 +0200

    Added Dtos and controllers to Accounts

---
+++ backend/Reactivities.Api/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Api.DTOs;
+using Reactivities.Domain;
+
+namespace Reactivities.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController(UserManager<AppUser> userManager) : ControllerBase
+{
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await userManager.FindByEmailAsync(loginDto.Email);
+
+        if (user == null) return Unauthorized();
+
+        var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+        if (result)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = "this will be a token",
+                UserName = user.DisplayName
+            };
+        }
+
+        return Unauthorized();
+    }
+}
